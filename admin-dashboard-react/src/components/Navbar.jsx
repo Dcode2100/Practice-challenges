28e52d0f67bd1a8b94135d07f60612decbffc340
Dikshant Solanki <dcode2100@gmail.com>
2023-04-02 17:12:44 +0530

    asdf

--- admin-dashboard-react/src/components/Navbar.jsx
+++ admin-dashboard-react/src/components/Navbar.jsx
@@ -1,55 +1,79 @@
 import React, { useState } from "react";
 import "./Navbar.css";
-import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
-
-const navitems = [
-  { name: "Home", path: "/" },
-  { name: "Shop", path: "#shop" },
-  { name: "Contact", path: "#contact" },
-  { name: "Login", path: "#login" },
-];
 
 const Navbar = () => {
-  const [active, setActive] = useState("Home");
-  const [nav, setNav] = useState(false);
+  const [hoveredItem, setHoveredItem] = useState(null);
 
-  const handleNavClick = () => {
-    setNav((prev) => !prev);
+  const navItems = [
+    {
+      id: 1,
+      title: "Home",
+      links: [
+        { id: 1, title: "Main", link: "/" },
+        { id: 2, title: "About", link: "/about" },
+        { id: 3, title: "Contact", link: "/contact" },
+      ],
+    },
+    {
+      id: 2,
+      title: "Products",
+      links: [
+        { id: 1, title: "All Products", link: "/products" },
+        { id: 2, title: "New Products", link: "/products/new" },
+        { id: 3, title: "Sale", link: "/products/sale" },
+      ],
+    },
+    {
+      id: 3,
+      title: "Services",
+      links: [
+        { id: 1, title: "Service 1", link: "/services/1" },
+        { id: 2, title: "Service 2", link: "/services/2" },
+        { id: 3, title: "Service 3", link: "/services/3" },
+      ],
+    },
+  ];
+
+  const handleHover = (itemId) => {
+    setHoveredItem(itemId);
   };
 
   return (
-    <div>
-      <div className="navbar">
-        <h1>Admin Dashboard</h1>
-        <div
-          className={`menu-icon ${nav ? "open" : ""}`}
-          onClick={handleNavClick}
-        >
-          {nav ? <CloseIcon /> : <MenuIcon />}
-        </div>
-        <div className="nav-dropdown">
-          {nav ? (
-            <ul className="nav-dropdown-links">
-              {navitems.map((item, index) => (
-                <li key={index} onClick={() => setActive(item.name)}>
-                  <a href={item.path}>{item.name}</a>
-                </li>
-              ))}
-            </ul>
-          ) : null}
-        </div>
-        <ul className="nav-links">
-          {navitems.map((item, index) => (
-            <li
-              key={index}
-              onClick={() => setActive(item.name)}
-              className={active === item.name ? "active" : ""}
-            >
-              <a href={item.path}>{item.name}</a>
-            </li>
-          ))}
-        </ul>
+    <div className="navbar-container">
+      <nav className="navbar">
+        {navItems.map((item) => (
+          <div
+            key={item.id}
+            className={`nav-item ${hoveredItem === item.id ? "hovered" : ""}`}
+            onMouseEnter={() => handleHover(item.id)}
+            onMouseLeave={() => handleHover(null)}
+          >
+            {item.title}
+          </div>
+        ))}
+      </nav>
+      <div className={`hover-block ${hoveredItem ? "show" : ""}`}>
+        {hoveredItem && (
+          <>
+            <div className="hover-block-left">
+              {navItems
+                .find((item) => item.id === hoveredItem)
+                .links.map((link) => (
+                  <div key={link.id} className="hover-block-link">
+                    <a href={link.link}>{link.title}</a>
+                  </div>
+                ))}
+            </div>
+            <div className="hover-block-right">
+              <img
+                src={`/${hoveredItem}.jpg`}
+                alt={`${
+                  navItems.find((item) => item.id === hoveredItem).title
+                } image`}
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
